0ce6e7b0dbc1d6fdbe17b032584be5de5c802618
U.Mukhin <u.mukhin@novacom.by>
2020-01-21 10:58:45 +0300

    add firebase auth

--- src/router.jsx
+++ src/router.jsx
@@ -3,23 +3,50 @@ import {
   HashRouter,
   Route,
   Switch,
+  Redirect,
 } from 'react-router-dom';
 
 import Layout from './containers/Layout';
 import Home from './containers/Home';
 import Settings from './containers/Settings';
 import Direction from './containers/Direction';
+import SignIn from './containers/SignIn';
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+} from "@react-firebase/auth";
+
+import { getFirebaseConfig } from './utils';
 
 const Router = () => (
-  <HashRouter>
-    <Layout>
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/settings" exact component={Settings}/>
-        <Route path="/:directionId(\d)" exact component={Direction}/>
-      </Switch>
-    </Layout>
-  </HashRouter>
+  <FirebaseAuthProvider {...getFirebaseConfig()} firebase={firebase}>
+    <HashRouter>
+      <FirebaseAuthConsumer>
+        {({ isSignedIn, user, providerId }) => {
+          console.log(isSignedIn, user);
+          if (isSignedIn) {
+            return (
+              <Layout>
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                  <Route path="/settings" exact component={Settings}/>
+                  <Route path="/:directionId(\d)" exact component={Direction}/>
+                </Switch>
+              </Layout>
+            )
+          }
+          return (
+            <Switch>
+              <Route path="/" exact component={SignIn}/>;
+            </Switch>
+          )
+        }}
+      </FirebaseAuthConsumer>
+    </HashRouter>
+  </FirebaseAuthProvider>
 );
 
 export default Router;
